4c314c551966813984ef7832db634573be10c61e
Vishwajeet KUmbhar <vishwajeetkumbhar97@gmail.com>
2025-12-05 09:59:38 +0530

    first commit

---
+++ src/test/java/jenkincicd/JenkincicdApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkincicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkincicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
